f1c8af659089039a22bf4fead8ee5e096527adda
dbarronsch <dbarron@barntownns.ie>
2026-06-01 13:32:23 +0100

    Update results-config.js

--- results-config.js
+++ results-config.js
@@ -1,1 +1,1 @@
-window.CLASSROOM_RESULTS_ENDPOINT = "";
+window.CLASSROOM_RESULTS_ENDPOINT = "https://defaultc44cdad8013c43038b7b38e3a7bd68.40.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/c9afbf8719664682ac2d154e05c41c3c/triggers/manual/paths/invoke?api-version=1";
